d05910ca73ba9c6825beead1f8cb33155c875e9a
Kim Yongbeom <dernbu@gmail.com>
2024-09-20 14:18:27 +0800

    feat(backend): Skeleton TypeScript server with Docker image

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3333, () => {
+    console.log('Server is running on localhost:3333');
+})
