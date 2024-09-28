6e5ef47ad522eb01278cc67aa5953c0e2d3fff9f
Suryansh Kushwaha <112935210+suryanshkushwaha@users.noreply.github.com>
2024-09-28 17:15:31 +0800

    add: template directories
    Created separate directories with templates for REST API implementation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
-import connectDB from './config/db';
+import connectDB from '../config/db';
 
 // initialize connection to MongoDB database
 connectDB()
