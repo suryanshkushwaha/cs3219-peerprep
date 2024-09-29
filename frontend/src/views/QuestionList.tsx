36cf43c68e9161070c7744425978bd247c86c9ee
Rubesh <rubesh77@gmail.com>
2024-09-29 19:13:50 +0800

    Remove commented code in Question List

--- frontend/src/views/QuestionList.tsx
+++ frontend/src/views/QuestionList.tsx
@@ -1,51 +1,3 @@
-/*import React from 'react';
-import { Question } from '../models/Question';
-
-interface QuestionListProps {
-  questions: Question[];
-  onDelete: (id: number) => void;
-  onEdit: (question: Question) => void;
-}
-
-const QuestionList: React.FC<QuestionListProps> = ({ questions, onDelete, onEdit }) => {
-  if (questions.length === 0) {
-    return <p>No questions available.</p>;
-  }
-
-  return (
-    <table className="question-table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Categories</th>
-          <th>Description</th>
-          <th>Difficulty</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {questions.map((question) => (
-          <tr key={question.id}>
-            <td>{question.id}</td>
-            <td>{question.title}</td>
-            <td>{question.categories.join(', ')}</td>
-            <td className="description-cell">{question.description}</td>
-            <td>{question.difficulty}</td>
-            <td>
-              <button className="edit-btn" onClick={() => onEdit(question)}>Edit</button>
-              <button className="delete-btn" onClick={() => onDelete(question.id)}>Delete</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
-
-export default QuestionList;
-*/
-
 import React from 'react';
 import { Question } from '../models/Question';
 
@@ -53,7 +5,7 @@ interface QuestionListProps {
   questions: Question[];
   onDelete: (id: number) => void;
   onEdit: (question: Question) => void;
-  onSelect: (question: Question) => void;  // Add this prop
+  onSelect: (question: Question) => void;
 }
 
 const QuestionList: React.FC<QuestionListProps> = ({ questions, onDelete, onEdit, onSelect }) => {
