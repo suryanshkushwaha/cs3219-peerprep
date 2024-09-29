b4ee0a618fb941d7255dc7ba03798973adca948a
Kim Yongbeom <dernbu@gmail.com>
2024-09-29 18:28:03 +0800

    fix: Fix dotenv file that should not be committed

--- backend/source.sh
+++ backend/source.sh
@@ -4,8 +4,6 @@ source ../source.sh
 
 set -o allexport
 
-source .env
-
 SERVICE_NAME=backend
 
 TF_VAR_service_name=${SERVICE_NAME}
@@ -13,6 +11,9 @@ DOCKER_IMAGE_NAME=${GCLOUD_REPOSITORY_URL}/${SERVICE_NAME}:$(git rev-parse HEAD)
 
 CLOUD_RUN_SERVICE_NAME=$(echo ${GCLOUD_PROJECT}-${SERVICE_NAME}-${ENV} | head -c 49) # Max length is 50 characters
 TF_VAR_cloud_run_service_name=${CLOUD_RUN_SERVICE_NAME}
+
+# MongoDB URI
+MONGODB_URI='mongodb+srv://common:7hdyD9fClPPeZuNa@cs3219questionservice.qwfia.mongodb.net/?retryWrites=true&w=majority&appName=CS3219QuestionService'
 
 EXPOSED_PORT=8080
 
