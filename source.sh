95495bdf0f90bdb5d92e3bcd36b02a61774692b1
Kim Yongbeom <dernbu@gmail.com>
2024-09-30 02:08:11 +0800

    chore: Remove .env from base dir

--- source.sh
+++ source.sh
@@ -3,8 +3,15 @@ PWD="$(git rev-parse --show-toplevel)"
 
 set -o allexport
 
-# Get the environment variables from the .env file in the root directory
-source "$PWD/.env"
+## Shared Environment Variables for the project
+GOOGLE_APPLICATION_CREDENTIALS=./secrets/gcp_credentials.json
+GCLOUD_PROJECT=cs3219-ay2425s1-project-g10
+GCLOUD_REGION=us-central1
+GCLOUD_ZONE=us-central1-c
+GCLOUD_REPOSITORY_ID=cs3219-ay2425s1-project-g10
+
+# Terraform backend variables
+TF_BACKEND_BUCKET_NAME=${GCLOUD_PROJECT}-tfstate
 
 GOOGLE_APPLICATION_CREDENTIALS=$(realpath "$PWD/$GOOGLE_APPLICATION_CREDENTIALS")
 
