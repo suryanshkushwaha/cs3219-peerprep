e7753587b09cdcd5470e5a8cd7173cbb28eb81fb
bhnuka <01bhanukaekanayake@gmail.com>
2024-11-07 12:02:59 +0800

    update naming for consistency

--- peerprep/backend/question-service/src/routes/testcaseRoutes.ts
+++ peerprep/backend/question-service/src/routes/testcaseRoutes.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import Testcase, { ITestcase } from '../models/Testcase';
+import Testcase, { ITestcase } from '../models/testcaseModel';
 
 const router = express.Router();
 
