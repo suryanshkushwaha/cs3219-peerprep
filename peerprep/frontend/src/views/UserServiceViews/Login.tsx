429f08359a81081c4acce97a897eebbd33258613
Kim Yongbeom <dernbu@gmail.com>
2024-11-11 20:43:47 +0800

    feat: Fix yarn build frontend

--- peerprep/frontend/src/views/UserServiceViews/Login.tsx
+++ peerprep/frontend/src/views/UserServiceViews/Login.tsx
@@ -1,5 +1,5 @@
 // src/Login.tsx
-import React, { useState } from "react";
+import { useState } from "react";
 import { loginUser } from "../../api/authApi";
 import { useNavigate, Link } from "react-router-dom"; // Import Link to navigate to signup
 
